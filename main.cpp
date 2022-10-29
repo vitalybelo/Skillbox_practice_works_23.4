28c9ece54ba8e8af9d9409dde2b4d5005ee9218e
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-10-29 16:15:35 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "windows.h"
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+
+
+
+
+
+
+    return 0;
+}
